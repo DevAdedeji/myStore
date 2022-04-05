3c2d7cc784644ffa0c7ef3333b2e33692bbd77d6
DevAdedeji <adedejitewogbade2@gmail.com>
2022-04-05 14:54:55 +0100

    Made Footer

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <Header />
+
   <router-view />
+  <Footer />
 </template>
 
 <script>
 import Header from "../src/components/Header.vue";
+import Footer from "../src/components/Footer.vue";
 export default {
   setup() {},
-  components: { Header },
+  components: { Header, Footer },
 };
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer>
+    <p>Made by <a href="https://devadedeji.github.io">Tewogbade Adedeji</a></p>
+  </footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+footer {
+  background: var(--primary-color);
+  text-align: center;
+  color: #fff;
+  padding: 10px;
+}
+</style>
